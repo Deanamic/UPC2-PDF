89a66285f51b35fa161f6f6a78c1d783e5f40448
Mackie Loeffel <mackie.loeffel@web.de>
2017-10-26 13:57:03 +0200

    added suffixarray and fixed whitespace

--- content/graph/globalmincut.h
+++ content/graph/globalmincut.h
@@ -34,62 +34,62 @@ bool a[NN];
 
 int pare[NN]; // **********
 int par (int b){ // **********
-    if (pare[b] == b) return b;
-    pare[b] = par(pare[b]);
-    return pare[b];
+	if (pare[b] == b) return b;
+	pare[b] = par(pare[b]);
+	return pare[b];
 }
 
 inline void merge (int b, int c){ // **********
-    pare[par(b)] = par(c);
+	pare[par(b)] = par(c);
 }
 
 pair < int, vi > minCut(vvi& g, int n) {
-    int n1 = n;
-  // init the remaining vertex set
-  for (int i = 0; i < n; i++){
-    v[i] = i;
-    pare[i] = i; // **********
-  }
-  // run Stoer-Wagner
-  int best = MAXW * n * n;
-  vi cut; // **********
-  while (n > 1) {
-    // initialize the set A and vertex weights
-    a[v[0]] = true;
-    for (int i = 1; i < n; i++) {
-      a[v[i]] = false;
-      w[i] = g[v[0]][v[i]];
-    }
-    // add the other vertices
-    int prev = v[0];
-    for (int i = 1; i < n; i++) {
-      // find the most tightly connected non-A vertex
-      int zj = -1;
-      for (int j = 1; j < n; j++)
-        if (!a[v[j]] && (zj < 0 || w[j] > w[zj])) zj = j;
-      // add it to A
-      a[v[zj]] = true;
-      // last vertex?
-      if (i == n - 1) {
-        // remember the cut weight
-        if (best > w[zj]){
-            best = w[zj];
-            cut.clear(); // ********
-            for (int ko = 0; ko < n1; ko++) if (par(ko) == par(v[zj])) cut.push_back(ko); // ********
-        }
+	int n1 = n;
+	// init the remaining vertex set
+	for (int i = 0; i < n; i++){
+		v[i] = i;
+		pare[i] = i; // **********
+	}
+	// run Stoer-Wagner
+	int best = MAXW * n * n;
+	vi cut; // **********
+	while (n > 1) {
+		// initialize the set A and vertex weights
+		a[v[0]] = true;
+		for (int i = 1; i < n; i++) {
+			a[v[i]] = false;
+			w[i] = g[v[0]][v[i]];
+		}
+		// add the other vertices
+		int prev = v[0];
+		for (int i = 1; i < n; i++) {
+			// find the most tightly connected non-A vertex
+			int zj = -1;
+			for (int j = 1; j < n; j++)
+				if (!a[v[j]] && (zj < 0 || w[j] > w[zj])) zj = j;
+			// add it to A
+			a[v[zj]] = true;
+			// last vertex?
+			if (i == n - 1) {
+				// remember the cut weight
+				if (best > w[zj]){
+					best = w[zj];
+					cut.clear(); // ********
+					for (int ko = 0; ko < n1; ko++) if (par(ko) == par(v[zj])) cut.push_back(ko); // ********
+				}
 
-        // merge prev and v[zj]
-        merge(prev, v[zj]); // *************
-        for (int j = 0; j < n; j++)
-          g[v[j]][prev] = g[prev][v[j]] += g[v[zj]][v[j]];
-        v[zj] = v[--n];
-        break;
-      }
-      prev = v[zj];
-      // update the weights of its neighbours
-      for (int j = 1; j < n; j++)
-        if (!a[v[j]]) w[j] += g[v[zj]][v[j]];
-    }
-  }
-  return {best, cut};
+				// merge prev and v[zj]
+				merge(prev, v[zj]); // *************
+				for (int j = 0; j < n; j++)
+					g[v[j]][prev] = g[prev][v[j]] += g[v[zj]][v[j]];
+				v[zj] = v[--n];
+				break;
+			}
+			prev = v[zj];
+			// update the weights of its neighbours
+			for (int j = 1; j < n; j++)
+				if (!a[v[j]]) w[j] += g[v[zj]][v[j]];
+		}
+	}
+	return {best, cut};
 }
