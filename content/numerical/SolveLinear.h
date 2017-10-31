a3734659b96b24ed7da346cd297c9886811401c0
Mackie Loeffel <mackie.loeffel@web.de>
2017-10-31 20:10:02 +0100

    added hint to gauss

--- content/numerical/SolveLinear.h
+++ content/numerical/SolveLinear.h
@@ -13,28 +13,29 @@ typedef vector<double> vd;
 const double eps = 1e-12;
 
 int solveLinear(vector<vd>& A, vd& b, vd& x) {
-	int n = sz(A), m = sz(x), rank = 0, br, bc;
-	if (n) assert(sz(A[0]) == m);
-	vi col(m); iota(all(col), 0);
+	int n = A.size(), m = x.size(), rank = 0, br, bc;
+	if (n) assert(A[0].size() == m);
+	// FOR(i, 0, n) FOR(j, 0, m) A[i][j] %= MOD; also b[i]...
+	vi col(m); iota(col.begin(), col.end(), 0);
 
-	rep(i,0,n) {
+	FOR(i,0,n) {
 		double v, bv = 0;
-		rep(r,i,n) rep(c,i,m)
+		FOR(r,i,n) FOR(c,i,m)
 			if ((v = fabs(A[r][c])) > bv)
 				br = r, bc = c, bv = v;
 		if (bv <= eps) {
-			rep(j,i,n) if (fabs(b[j]) > eps) return -1;
+			FOR(j,i,n) if (fabs(b[j]) > eps) return -1;
 			break;
 		}
 		swap(A[i], A[br]);
 		swap(b[i], b[br]);
 		swap(col[i], col[bc]);
-		rep(j,0,n) swap(A[j][i], A[j][bc]);
+		FOR(j,0,n) swap(A[j][i], A[j][bc]);
 		bv = 1/A[i][i];
-		rep(j,i+1,n) {
+		FOR(j,i+1,n) {
 			double fac = A[j][i] * bv;
 			b[j] -= fac * b[i];
-			rep(k,i+1,m) A[j][k] -= fac*A[i][k];
+			FOR(k,i,m) A[j][k] -= fac*A[i][k];
 		}
 		rank++;
 	}
@@ -43,7 +44,7 @@ int solveLinear(vector<vd>& A, vd& b, vd& x) {
 	for (int i = rank; i--;) {
 		b[i] /= A[i][i];
 		x[col[i]] = b[i];
-		rep(j,0,i) b[j] -= A[j][i] * b[i];
+		FOR(j,0,i) b[j] -= A[j][i] * b[i];
 	}
 	return rank; // (multiple solutions if rank < m)
 }
