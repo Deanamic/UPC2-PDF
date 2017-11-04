cbb610572887b4cd701fbba31fceb05a7da8f5a3
Eric Valls <eric.vallsg@gmail.com>
2017-11-04 19:01:28 +0100

    New version of suffix array

--- content/strings/SuffixArray.h
+++ content/strings/SuffixArray.h
@@ -1,79 +1,79 @@
-/**
- * Author: Lukas Polacek
- * Date: 2009-10-27
- * License: CC0
- * Source: folklore and Linear-time longest-common-prefix
- * computation in suffix arrays and its applications (2001).
- * Description: Builds suffix array for a string. $a[i]$ is
- * the starting index of the suffix which is $i$-th in the
- * sorted suffix array. The returned vector is of size $n+1$,
- * and $a[0] = n$. The {\tt lcp} function calculates longest
- * common prefixes for neighbouring strings in suffix array.
- * The returned vector is of size $n+1$, and $ret[0] = 0$.
- * Time: $O(N \log^2 N)$ where $N$ is the length of the string
- * for creation of the SA. $O(N)$ for longest common prefixes.
- * Memory: $O(N)$
- * Status: Tested on UVa Live 4513
- */
-#pragma once
-typedef pair<ll, int> pli;
-void count_sort(vector<pli> &b, int bits) { // (optional)
-	//this is just 3 times faster than stl sort for N=10^6
-	int mask = (1 << bits) - 1;
-	FOR(it,0,2) {
-		ll move = it * bits;
-		vi q(1 << bits), w(q.size() + 1);
-		FOR(i,0,b.size())
-			q[(b[i].first >> move) & mask]++;
-		partial_sum(q.begin(), q.end(), w.begin() + 1);
-		vector<pli> res(b.size());
-		FOR(i,0,b.size())
-			res[w[(b[i].first >> move) & mask]++] = b[i];
-		swap(b, res);
+/* Suffix Array + LCP
+ lcp (x,y) = min(lcp(x,x+1),lcp(x+1,x+2)...lcp(y-1,y)) to answer queries with RMQ O(1)
+ Complexity:
+ 	- Time:
+		Build O(nlogn)
+		LCP O(logn)
+	- Memory
+		Build O(nlogn) (Can be optimised as in B you only use the previous row to compute one row. But then you cannot do lcp
+Source: PDF UPC
+Tested on https://www.codechef.com/problems/TASTR
+
+NOTE: It is not necessary to use raddixsort if the O(nlog^2 n) fits the time limit, one can just use stl sort
+*/
+
+struct SF {
+  pair<ll, ll> ord;
+  ll id;
+  bool operator<(const SF& s) const { return ord < s.ord; }
+};
+ 
+ 
+ 
+ll lcp(ll x, ll y, vector < vector < ll > > &B, ll N, ll step) {
+  if (x == y) return N - x;
+  ll res = 0;
+  for (ll i = step - 1; i >= 0 and x < N and y < N; --i)
+    if (B[i][x] == B[i][y]) { x += 1<<i; y += 1<<i; res += 1<<i; }
+  return res;
+}
+
+ 
+ 
+ 
+void raddixSort(vector < SF > & A, vector < vector <ll > > & B, vector < ll > &times, vector < ll > & pos, vector < SF > & L2, ll N) {
+  ll k = max(N, 256LL);
+  for (ll i = 0; i < k + 2; ++i) times[i] = 0;
+  for (ll i = 0; i < N; ++i)
+    times[A[i].ord.second + 1]++;
+  pos[0] = 0;
+  for (ll i = 1; i < k + 2; ++i)
+    pos[i] = pos[i - 1] + times[i - 1];
+  for (ll i = 0; i < N; ++i)
+    L2[pos[A[i].ord.second + 1]++] = A[i];
+ 
+  for (ll i = 0; i < k + 2; ++i)
+    times[i] = 0;
+  for (ll i = 0; i < N; ++i)
+    times[L2[i].ord.first + 1]++;
+  pos[0] = 0;
+  for (ll i = 1; i < k + 2; ++i)
+    pos[i] = pos[i - 1] + times[i - 1];
+  for (ll i = 0; i < N; ++i)
+    A[pos[L2[i].ord.first + 1]++] = L2[i];
+}
+ 
+ 
+ 
+void compute_suffix_array(vector < SF > & A, vector<vector<ll> > & B, ll N, string & S, ll &step) {
+	ll MAXN = 3000005; //millor posar numero gran que algo en funcio de N pq peta
+	vector < SF > L2(MAXN);
+	vector < ll > pos(MAXN + 2,0), times(MAXN + 2,0);
+	A.resize(N); B.resize(1); B[0].resize(N);
+	for (ll i = 0; i < N; ++i) B[0][i] = S[i];
+	step = 1;
+	for (ll b = 0, pw = 1; b < N; ++step, pw<<=1) {
+		for (ll i = 0; i < N; ++i) {
+			A[i].ord.first = B[step - 1][i];
+			A[i].ord.second = i + pw < N ? B[step - 1][i + pw] : -1;
+			A[i].id = i;
+		}
+		raddixSort(A, B, times, pos, L2, N); //sort(A.begin(), A.end());
+		B.resize(step + 1); B[step].resize(N);
+		b = B[step][A[0].id] = 1;
+		for (ll i = 1; i < N; ++i) {
+			if (A[i - 1] < A[i]) ++b;
+			B[step][A[i].id] = b;
+		}
 	}
 }
-struct SuffixArray {
-	vi a;
-	string s;
-	SuffixArray(const string& _s) : s(_s + '\0') {
-		int N = s.size();
-		vector<pli> b(N);
-		a.resize(N);
-		FOR(i, 0, N) {
-			b[i].first = s[i];
-			b[i].second = i;
-		}
-		int q = 8;
-		while((1 << q) < N) q++;
-		for(int moc = 0; ; moc++) {
-			count_sort(b, q); // sort(b.begin(), b.end()) can be used as well
-			a[b[0].second] = 0;
-			FOR(i, 1, N) {
-				a[b[i].second] = a[b[i-1].second]
-					+ (b[i-1].first != b[i].first);
-			}
-			if((1 << moc) >= N) break;
-			FOR(i, 0, N) {
-				b[i].first = (ll)a[i] << q;
-				if(i + (1 << moc) < N) {
-					b[i].first += a[i + (1 << moc)];
-				}
-				b[i].second = i;
-			}
-		}
-		FOR(i, 0, a.size()) a[i] = b[i].second;
-	}
-
-	vi lcp() {
-		int n = a.size(), h = 0;
-		vi inv(n), res(n);
-		FOR(i, 0, n) inv[a[i]] = i;
-		FOR(i, 0, n) if (inv[i] > 0) {
-			int p0 = a[inv[i] - 1];
-			while(s[i+h] == s[p0+h]) h++;
-			res[inv[i]] = h;
-			if(h > 0) h--;
-		}
-		return res;
-	}
-};
