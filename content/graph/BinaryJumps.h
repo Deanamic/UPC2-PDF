c8a28039ec1a0c31f566f0123f42addfdb70b27f
Mackie Loeffel <mackie.loeffel@web.de>
2017-11-19 16:49:21 +0100

    binary jumps with insert

--- content/graph/BinaryJumps.h
+++ content/graph/BinaryJumps.h
@@ -1,85 +1,37 @@
 /**
- * Author: Dean
+ * Author: Dean & Michael
  * License: CC0
- * Description: Computes lca with binaryjumps, root is 0
- * Time: O(nlogn)
- * Status: Tested?
+ * Description: Computes lca with binaryjumps. Insert nodes in dfs preorder
+ * Time: O(n \log n)
+ * Status: Tested on http://codeforces.com/contest/757/problem/F
  */
 
+const ll maxN = 201005; // TODO
+const ll MAXLOG = 20; // TODO: (1 << MAXLOG) > maxN
 struct BinaryLifting {
-    vector<vector<int> > BL;
-    vector<vector<int> > adj;
-    vector<int> P;
-    vector<int> D;
-    int V,E;
-    int MAXLOG = 0;
+    ll BL[maxN][MAXLOG];
+    ll D[maxN];
 
-    void dfs(int u, int p){
-        D[u] = D[p] + 1;
-        for (int v : adj[u]) {
-            if (v != p) {
-                BL[v][0] = u;
-                dfs(v, u);
-            }
-        }
-    }
-
-    void build(int n){
-        V = n;
-        E = n-1;
-        P.resize(V+1);
-        D.resize(n);
-        while ((1 << MAXLOG) <= V) ++MAXLOG;
-        BL = vector<vector<int> > (V, vector<int> (MAXLOG));
-        adj.resize(V);
-        for (int i = 0; i < E; ++i) {
-            int x, y;
-            cin >> x >> y;
-            adj[x].push_back(y);
-            adj[y].push_back(x);
-        }
-        D[0] = -1;
-        dfs(0,0);
-        int LOG = 0;
-
-        for (int i = 1; i < V + 1; ++i) {
-            if(1 << (LOG+1) <= i) P[i] = ++LOG;
-            else P[i] = LOG;
-        }
-
-        for (int j = 1; j < MAXLOG; ++j) {
-            for (int i = 0; i < V; ++i) {
-                BL[i][j] = BL[BL[i][j-1]][j-1];
-            }
-        }
-    }
-
-    int maxjump(int depth, int node){
-        return BL[node][P[depth]];
-    }
-
-    int lift(int node, int h) {
-        int cnt = 0;
+	void init(ll root) { D[root] = 1; }
+	void insert(ll n, ll par) {
+		D[n] = D[par] + 1;
+		BL[n][0] = par;
+		FOR(j, 1, MAXLOG) BL[n][j] = BL[BL[n][j-1]][j-1];
+	}
+	ll lift(ll node, ll h) {
+        ll cnt = 0;
         while(h) {
             if(h&1) node = BL[node][cnt];
-            cnt++;
-            h >>= 1;
+            cnt++; h >>= 1;
         }
         return node;
     }
-
-    int LCA(int a, int b) {
-        if(D[a] > D[b]) a = lift(a, D[a] - D[b]);
-        else b = lift(b, D[b] - D[a]);
-        if(a == b) return a;
-        int jump = MAXLOG-2;
-        while(jump && a != b) {
-            if(BL[a][jump] != BL[b][jump] && BL[a][jump+1] != BL[b][jump+1]) {
-                a = lift(a, jump);
-                b = lift(b, jump);
-            }
-            --jump;
-        }
-        return BL[a][0];
+    int lca(int a, int b) {
+		if(D[a] > D[b]) swap(a, b);
+		FORD(i, 0, MAXLOG) if(D[BL[b][i]] >= D[a]) b = BL[b][i];
+		if(a == b) return a;
+		FORD(i, 0, MAXLOG) if(BL[a][i] != BL[b][i])
+			a = BL[a][i], b = BL[b][i];
+		return BL[a][0];
     }
 };
