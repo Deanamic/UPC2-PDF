2613c6061e0f39f5feb0d3116bb5b060579a8e7d
Dean Zhu <deanzhu1@gmail.com>
2017-11-22 10:45:28 +0100

    added change to template

--- content/contest/template.cpp
+++ content/contest/template.cpp
@@ -11,7 +11,7 @@ const ll oo = 0x3f3f3f3f3f3f3f3fLL;
 #define FORD(i, a, b) for(ll i = (b)-1; i >= int(a); i--)
 #define has(c, e) ((c).find(e) != (c).end())
 #define sz(c) ll((c).size())
-
+#define all(c) c.begin(), c.end()
 int main() {
 	cin.sync_with_stdio(0); cin.tie(0);
 	cin.exceptions(cin.failbit);
