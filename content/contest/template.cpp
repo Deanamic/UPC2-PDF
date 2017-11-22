9b1ac96c7861b10bb0f3bd1f196f1588eeca5911
Dean Zhu <deanzhu1@gmail.com>
2017-11-22 10:59:01 +0100

    removed mergec cponflict

--- content/contest/template.cpp
+++ content/contest/template.cpp
@@ -11,12 +11,8 @@ const ll oo = 0x3f3f3f3f3f3f3f3fLL;
 #define FORD(i, a, b) for(ll i = (b)-1; i >= int(a); i--)
 #define has(c, e) ((c).find(e) != (c).end())
 #define sz(c) ll((c).size())
-<<<<<<< HEAD
 #define all(c) c.begin(), c.end()
-=======
-#define all(c) c.begin(),c.end()
 
->>>>>>> 12a72bedf6d66d69e292b7c49561e9809488876d
 int main() {
 	cin.sync_with_stdio(0); cin.tie(0);
 	cin.exceptions(cin.failbit);
